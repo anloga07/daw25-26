867912cc478429782dabbc5d8d953705de09f3f8
Angel <anloga07@gmail.com>
2026-01-30 13:09:22 +0100

    layout con producto

--- daw25-26/src/app/app.ts
+++ daw25-26/src/app/app.ts
@@ -2,17 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { About } from './pages/about/about';
-import { Productos } from './pages/productos/productos';
-import { Contacto } from './pages/contacto/contacto';
-import { Home } from './pages/home/home';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,Footer,Home ,About,Productos,Contacto],
+  imports: [RouterOutlet,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('daw25-26'); 
+  //title="Proyecto uno"
+
 }
--- daw25-26/src/app/pages/productos/productos.ts
+++ daw25-26/src/app/pages/productos/productos.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './productos.css',
 })
 export class Productos {
-
+     title = 'Nuestros productos';
+     // Propiedades de producto
+     nombreProducto = 'Portátil Gaming MSI';
+     precio = 1299.99;
+     stock = 15;
+     disponible = true;
+     descripcion = 'Portátil de alto rendimiento con procesador Intel i7 y tarjeta gráfica RTX 4060';
+     // Método para calcular precio con IVA
+     calcularPrecioConIVA(): number {
+        return this.precio * 1.21;
+     }
+     // Método para obtener estado de stock
+     obtenerEstadoStock(): string {
+        if (this.stock > 10) {
+            return 'Stock alto';
+     } else if (this.stock > 0) {
+         return 'Últimas unidades';
+     } else {
+        return 'Agotado';
+     }
+  }
 }
